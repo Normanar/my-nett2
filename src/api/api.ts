c55867501816e905033adf4c9df793d1ba317c39
Arman Alimov <527c@mail.ru>
2021-11-15 21:03:15 +0600

    change axios on instance

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,20 @@
 import axios from "axios";
 
+const instance = axios.create( {
+    baseURL: "https://social-network.samuraijs.com/api/1.0/",
+    withCredentials: true,
+    headers: {
+        'API-KEY': 'eaf34c04-f789-4b8a-b729-c59d43de7ca7'
+    }
+} )
 
 export const usersAPI = {
     getUsers(currentPage: number, pageSize: number) {
-        return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
-            withCredentials: true
-        })
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
     },
 
     getProfileOfUser(userID: string) {
-        return axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userID}`)
+        return instance.get(`profile/${userID}`)
     },
 
     isLoginIn() {
