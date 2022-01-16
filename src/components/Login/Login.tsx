a2ba56c3d226701d58458cd4fc096a81df3c1489
Arman Alimov <527c@mail.ru>
2022-01-16 23:06:58 +0600

    add yup

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import g from "./Login.module.css"
 
+type MyFormValues = {
+    email: string
+    password: string
+    rememberMe: boolean
+}
+
 export const Login = () => {
+
+    const initialValues: MyFormValues = {
+        email: "",
+        password: "",
+        rememberMe: false
+    };
+
     return (
         <div className={g.loginBlock}>
             LOGIN
