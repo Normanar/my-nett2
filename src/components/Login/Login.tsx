c62c7e02e90b739ee335c0ec5e6be998014d8183
Arman Alimov <527c@mail.ru>
2022-01-16 23:19:18 +0600

    add input email password in login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import g from "./Login.module.css"
+import g from "./Login.module.css";
+import * as yup from "yup";
+import {Field, Form, Formik, ErrorMessage} from "formik";
 
 type MyFormValues = {
     email: string
@@ -15,9 +17,58 @@ export const Login = () => {
         rememberMe: false
     };
 
+    const validation = yup.object().shape({
+        email: yup.string().email("Please enter a valid email address").required("Required"),
+        password: yup.string().required("Required")
+    })
+
     return (
         <div className={g.loginBlock}>
-            LOGIN
+            <div>Login</div>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validation}
+                onSubmit={(values, actions) => {
+                    console.log(values);
+                    actions.setSubmitting(false);
+                }}
+            >
+                {({
+                      isSubmitting,
+                      dirty,
+                      isValid,
+                  }) => (
+                    <Form>
+                        <div>
+                            <Field
+                                type="email"
+                                name="email"
+                                placeholder={"Email"}
+                            />
+                            <ErrorMessage name="email" component="span" className={g.error}/>
+                        </div>
+                        <div>
+                            <Field
+                                type="password"
+                                name="password"
+                                placeholder={"Password"}
+                            />
+                            <ErrorMessage name="password" component="div" className={g.error}/>
+                        </div>
+                        <div>
+                            <Field
+                                type="checkbox"
+                                name="rememberMe"
+                            />
+                        </div>
+                        <div>
+                            <button type="submit" disabled={!isValid || isSubmitting || !dirty}>
+                                Submit
+                            </button>
+                        </div>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }
