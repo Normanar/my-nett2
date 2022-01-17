1c12e15dcbeb4ef11c5434606c48fe6881cfba05
Arman Alimov <527c@mail.ru>
2022-01-17 23:06:53 +0600

    add react icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import g from './Header.module.css'
 import {HeaderContainerWithAxiosPropsType} from "./HeaderContainer";
+import signOut from "../../images/22.png"
+
 
 type HeaderPropsType = HeaderContainerWithAxiosPropsType
 
@@ -10,9 +12,9 @@ const Header: React.FC<HeaderPropsType> = (props) => {
         <header className={g.header}>
             <div className={g.header_text}>FriendFace /</div>
             {props.isAuth
-                ? <div className={g.loginBlock}>{props.login}</div>
+                ? <span><div className={g.loginBlock}>{props.login}</div><img src={signOut} className={g.sign_out}/></span>
                 : <NavLink to={"/login"}>
-                    <div className={g.loginBlock}>Login</div>
+                    <div className={g.loginBlock}>Sign In</div>
                 </NavLink>
             }
         </header>
