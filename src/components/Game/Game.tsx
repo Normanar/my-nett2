16a27fc1994e7ce513c651473dce5057cc6d67e4
Arman Alimov <527c@mail.ru>
2022-01-30 22:27:45 +0600

    show More, show Less, show Win

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 import g from "./Game.module.css"
 
 
@@ -6,9 +6,21 @@ export function Game () {
 
     const [num, setNum] = useState<number>(0)
     const [show, setShow] = useState<boolean>(false)
+    const [numOfPlayer, setNumOfPlayer] = useState<number>(0)
+    const [showMore, setShowMore] = useState<boolean>(false)
+    const [showLess, setShowLess] = useState<boolean>(false)
+    const [showWin, setShowWin] = useState<boolean>(false)
 
     const onClickStartButton = () => {
         setNum(+Math.random().toFixed(2) * 100 )
+    }
+
+    const onChangeInput = (e:ChangeEvent<HTMLInputElement>) => {
+        setNumOfPlayer(+e.target.value)
+    }
+
+    const onClickButtonGo = () => {
+
     }
 
     return (
@@ -19,8 +31,8 @@ export function Game () {
                 <button onClick={onClickStartButton}>Start</button>
                 {num}
                 <div>
-                    <input type={"number"}/>
-                    <button>Go</button>
+                    <input type={"number"} value={numOfPlayer} onChange={onChangeInput}/>
+                    <button onClick={onClickButtonGo}>Go</button>
                     <div>More</div>
                     <div>Less</div>
                 </div>
