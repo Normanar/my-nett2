eca5cb89c0ebf0d270527d7178c066586206dea1
Arman Alimov <527c@mail.ru>
2022-01-30 22:19:59 +0600

    game component structure

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import g from "./Game.module.css"
 
 
 export function Game () {
+
+    const [num, setNum] = useState<number>(0)
+    const [show, setShow] = useState<boolean>(false)
+
+    const onClickStartButton = () => {
+        setNum(+Math.random().toFixed(2) * 100 )
+    }
+
     return (
         <div className={g.game_block}>
             <div className={g.game_container}>
                 <div>Game</div>
                 <div>You need to guess the given number. You have five attempts.</div>
-                <button>Start</button>
+                <button onClick={onClickStartButton}>Start</button>
+                {num}
+                <div>
+                    <input type={"number"}/>
+                    <button>Go</button>
+                    <div>More</div>
+                    <div>Less</div>
+                </div>
             </div>
         </div>
     )
