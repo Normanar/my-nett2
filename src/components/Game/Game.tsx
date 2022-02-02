fb6d6b83e900e09bda3d69bd4c2f974054f0f290
Arman Alimov <527c@mail.ru>
2022-02-02 14:29:41 +0600

    add in Game lose condition

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -2,7 +2,7 @@ import React, {ChangeEvent, useState} from "react";
 import g from "./Game.module.css"
 
 
-export function Game () {
+export function Game() {
 
     const [num, setNum] = useState<number>(0)
     const [show, setShow] = useState<boolean>(false)
@@ -10,6 +10,7 @@ export function Game () {
     const [showMore, setShowMore] = useState<boolean>(false)
     const [showLess, setShowLess] = useState<boolean>(false)
     const [showWin, setShowWin] = useState<boolean>(false)
+    const [showLose, setShowLose] = useState<boolean>(false)
     const [round, setRound] = useState(0)
 
     const countOfRound = round < 5
@@ -18,9 +19,11 @@ export function Game () {
         setNum(Math.floor(Math.random() * 100) + 1)
         setShow(true)
         setShowWin(false)
+        setShowLose(false)
+        setRound(0)
     }
 
-    const onChangeInput = (e:ChangeEvent<HTMLInputElement>) => {
+    const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
         setNumOfPlayer(e.target.value)
     }
 
@@ -29,14 +32,19 @@ export function Game () {
         if (num === +numOfPlayer) {
             setShowWin(true)
             setNumOfPlayer('')
+        } else if (round >= 4) {
+            setShowLose(true)
+            setNumOfPlayer('')
         } else if (num > +numOfPlayer) {
             setShowMore(true)
             setNumOfPlayer('')
+            setRound(round + 1)
         } else {
             setShowLess(true)
             setNumOfPlayer('')
+            setRound(round + 1)
         }
-        setRound(round + 1)
+
     }
 
     const onFocusInput = () => {
@@ -47,21 +55,29 @@ export function Game () {
     return (
 
 
-
         <div className={g.game_block}>
             <div className={g.game_container}>
                 <div>Game</div>
                 <div>You need to guess the given number: from 1 to 100. You have five attempts.</div>
                 <button onClick={onClickStartButton}>New game</button>
-                {/*{num}*/}
+                {num}
                 <div>
-                    {show && countOfRound && <input type={"text"} value={numOfPlayer} onChange={onChangeInput} onFocus={onFocusInput}/>}
-                    {show && countOfRound && <button onClick={onClickButtonGo}>Go</button>}
-                    { showMore && countOfRound && <div>More</div> }
-                    { showLess && countOfRound && <div>Less</div> }
-                    { showWin && countOfRound && <div>You win!!!</div>}
+                    {show && <input
+                        type={"text"}
+                        value={numOfPlayer}
+                        onChange={onChangeInput}
+                        onFocus={onFocusInput}
+                        className={g.input}
+                    />}
+                    {show && <button onClick={onClickButtonGo}>Go</button>}
+                    {showMore && countOfRound && <div>More</div>}
+                    {showLess && countOfRound && <div>Less</div>}
+                    {showWin && countOfRound && <div>You win!</div>}
+                    {showLose && <div>{`Maybe next time! It was ${num}`}</div>}
                 </div>
             </div>
+            {console.log(round)}
+            {console.log(112121212121212121212)}
         </div>
     )
 }
